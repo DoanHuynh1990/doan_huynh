587d3b3f47f8e6540e9aeaa7177e4f59759163c1
DoanHuynh1990 <longway1990@gmail.com>
2025-03-06 19:48:45 +0700

    Tìm số lớn nhất của x,y

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -1,5 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+// vung ben ngoai ham
+//int bien_toan_cuc;	
 void main()
 {
-	printf("Xin chao \n");
+	//vung ben trong ham
+	//unsigned char x = 1233;
+	int x = 10;
+	int y = 13;
+
+	printf("nhap gia tri cho x:");
+	scanf_s("%d", &x);
+
+	printf("nhap gia tri cho y:");
+	scanf("%d", &y);
+
+	if (x > y)
+	{
+		printf("Gia tri x:%d \n", x);
+
+	}
+	else
+	{
+		printf("Gia tri y:%d \n", y);
+	}
+
+	
+	
+
+
+	//printf("Gia tri x:%d \n",x);
 }
