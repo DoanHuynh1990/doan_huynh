d2e2090e2dd47758a48ec885be8cde212656e2da
DoanHuynh1990 <longway1990@gmail.com>
2025-03-10 21:35:08 +0700

    Bai toan ATM

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -3,39 +3,54 @@
 
 void main()
 {
-
-	printf("Xep loai hoc sinh dua vao diem TB\n");
-
-	float diemTB = 10;
-
-
-	printf("nhap gia tri cho diemTB:");
-	scanf_s("%f", &diemTB);
-
-
-
-	if (diemTB  >= 8.0)
+	int buoc = 1;
+	int mat_khau = 1234;
+	int mat_khau_user = 0;
+	int so_lan_nhap_sai = 0;
+	int so_tien_can_rut = 0;
+	while (buoc != 5)
 	{
-		printf("Loai : Gioi");
+
+		switch (buoc)
+		{
+		case 1:
+			printf("vui long nhap mat khau: ");
+			scanf("%d", &mat_khau_user);
+			if (mat_khau_user!=mat_khau)
+			{
+				so_lan_nhap_sai++;
+				if (so_lan_nhap_sai>=3)
+				{
+					buoc = 5;
+
+				}
+			}
+			else
+			{
+				buoc = 2;
+			}
+			break;
+
+		case 2:
+			printf("vui long nhap so tien cat rut: ");
+			scanf("%d", &so_tien_can_rut);
+			if (so_tien_can_rut>10000)
+			{
+				buoc = 2;
+			}
+			else
+			{
+				buoc = 3;
+			}
+			break;
+		case 3:
+			printf("so tien ban rut la:%d\n",so_tien_can_rut);
+			buoc = 4;
+			break;
+
+		}
 
 	}
-	else if (diemTB >=6.5)
-	{
-		printf("Loai : Kha");
-	}
-	else if (diemTB >= 5.0)
 
-	{
-		printf("Loai : Trung BInh");
-	}
-	else
-	{
-		printf("Loai : Yeu");
-	}
-
-	
-	
-
-
-	//printf("Gia tri x:%d \n",x);
 }
+
