63e45662e27bbabdf532db4579e4f85a8e00c51c
DoanHuynh1990 <longway1990@gmail.com>
2025-03-11 19:57:43 +0700

    Bang cuu chuong

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -3,54 +3,22 @@
 
 void main()
 {
-	int buoc = 1;
-	int mat_khau = 1234;
-	int mat_khau_user = 0;
-	int so_lan_nhap_sai = 0;
-	int so_tien_can_rut = 0;
-	while (buoc != 5)
-	{
+    int a, b, cout;
+    int st, ed;
 
-		switch (buoc)
-		{
-		case 1:
-			printf("vui long nhap mat khau: ");
-			scanf("%d", &mat_khau_user);
-			if (mat_khau_user!=mat_khau)
-			{
-				so_lan_nhap_sai++;
-				if (so_lan_nhap_sai>=3)
-				{
-					buoc = 5;
+    st = 2, ed = 9;
 
-				}
-			}
-			else
-			{
-				buoc = 2;
-			}
-			break;
+    printf("In bang cuu chuong:\n");
+    for (a = st; a <= ed; a++) {
+        cout = a;
 
-		case 2:
-			printf("vui long nhap so tien cat rut: ");
-			scanf("%d", &so_tien_can_rut);
-			if (so_tien_can_rut>10000)
-			{
-				buoc = 2;
-			}
-			else
-			{
-				buoc = 3;
-			}
-			break;
-		case 3:
-			printf("so tien ban rut la:%d\n",so_tien_can_rut);
-			buoc = 4;
-			break;
+        for (b = 1; b <= 10; b++) {
+            printf(" %3d", cout * b);
+        }
 
-		}
+        printf("\n");
+    }
 
-	}
-
+  
 }
 
