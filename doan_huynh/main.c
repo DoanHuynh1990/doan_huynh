ce677c888ea8eb8eea9861b944f7984df455ae4c
DoanHuynh1990 <longway1990@gmail.com>
2025-03-12 20:37:31 +0700

    Nhap so 0

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -3,36 +3,15 @@
 
 void main()
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int d = 0;
-	int so_tien_can_tra = 0;
-	int change = 0;
+	int a = 1;
+
 
 	do
 	{
-		printf("Nhap so tien can rut: ", so_tien_can_tra);
-		scanf_s("%d", &so_tien_can_tra);
-		if ((so_tien_can_tra % 50000) != 0)
-		{
-			printf("SO TIEN CAN RUT PHAI LA BOI SO CUA 50k\n");
-		}
-
-	} while ((so_tien_can_tra%50000)!=0);
-
-
-		change = so_tien_can_tra / 50000;
-		a = change / 17;
-		b = (change % 17)/7;
-		c = ((change % 17) % 7) / 3;
-		d = (((change % 17) % 7) % 3)/1;
-		printf("So to 500k: %d\n", a);
-		printf("So to 200k: %d\n", a+b);
-		printf("So to 100k : %d\n", a + b + c);
-		printf("So to 50k: %d\n", a + b + c + d);
-
-
+		printf("Nhap vao man hinh: ", a);
+		scanf_s("%d", &a);
+	
+	} while (a!=0);
 
 
 }
