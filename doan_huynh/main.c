f04c8b4c07f28d64b18127697d122e7aeca9894c
DoanHuynh1990 <longway1990@gmail.com>
2025-03-13 20:56:42 +0700

    Dem ky tu chuoi

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -2,24 +2,16 @@
 #include<stdio.h>
 int main()
 {
-	//khai bao 10 doi tuong kieu iteger
+	/// Khai bao chuoi Hello
 
-	int arr_1[10] = {0};   // cach 1
-	int tong = 0;
-	for ( int i = 0; i < 10; i++)
+	char str_3[] = "xin chao";
+	int dem = 0;
+
+	for (int i = 0; str_3[i] != 0; i++)
 	{
-		printf("Nhap num %d : ",i);
-		scanf_s("%d", &arr_1[i]);
-
+		dem++;
 	}
-	for (int j = 0; j < 9; j++)
-	{
-		tong += arr_1[j];
 
-	}
-	printf("Tong :%d", tong);
-
-
-
+	printf("dem: %d", dem);
 
 }	
