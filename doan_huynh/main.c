7a7fa9c8daf33a6c189976dc5c513fa1e9bc78fa
DoanHuynh1990 <longway1990@gmail.com>
2025-03-15 15:15:48 +0700

    Array_Bai4

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -2,22 +2,20 @@
 #include<stdio.h>
 int main()
 {
-	int arr[] = { 1,2,4,5,3,455,5,34,9 };
+	int arr[] = { 1,2,-4,5,3,455,-6,34,9 };
+	int loop;
 	int arr_size = sizeof(arr) / sizeof(arr[0]);
-	int odd_nums = 0;
-	int odd_total = 0;
-	float odd_ave = 0;
 
-		for (int i = 0; i < arr_size; i++)
+	for (int i = 0; i < arr_size; i++)
+	{
+		if (arr[i]<0)
 		{
-			if ((arr[i] % 2) != 0)
-				odd_nums++;
-			    odd_total += arr[i];
-
+			arr[i] = 0;
 		}
-		odd_ave = odd_total / odd_nums;
-		printf("Tong so le: %d", odd_total);
-		printf("Trung binh cong so le: %f", odd_ave);
+		
+	}
+	printf("In tat ca phan tu cua mang: \n\n");
+	for (loop = 0; loop < arr_size; loop++)
+		printf("%d ", arr[loop]);
 
-	
 }	
