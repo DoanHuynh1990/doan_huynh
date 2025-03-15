d28b120fa334fa57dae9d23fea2bbd8412f0ec91
DoanHuynh1990 <longway1990@gmail.com>
2025-03-15 15:25:26 +0700

    Array_Bai7

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -4,12 +4,16 @@ int main()
 {
 	int arr[] = { 1,2,-4,5,3,455,-6,34,9 };
 	int arr_size = sizeof(arr) / sizeof(arr[0]);
+	int max_value = 0;
 
 	for (int i = 0; i < arr_size; i++)
 	{
-		if (arr[i]%2!=0)
+		if (max_value < arr[i])
 		{
-			printf("%d", i);
+			max_value = arr[i];
 		}
+		
 	}
+
+	printf("So lon nhat : %d", max_value);
 }	
