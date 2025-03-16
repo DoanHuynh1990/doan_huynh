41a6a948830e392677dc8a9b9136add582726681
DoanHuynh1990 <longway1990@gmail.com>
2025-03-16 20:17:36 +0700

    String_Bai5

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -2,22 +2,24 @@
 #include<stdio.h>
 int main()
 {
-	char str[] = "Xin Chao moi nguoi ^%$";
+	char str[] = "xin chao moi nguoi ^%$";
 	int str_len = 0;
 	int Space_num = 0;
 
 	while (str[str_len] != 0) {
 		str_len++;
 	}
-	for (int i = 0; i < str_len; i++)
+	for (int i = 1; i < str_len; i++)
 	{
-		if (str[i]==32)
+		if (str[0] >= 'a' && str[0] <= 'z')
 		{
-			Space_num++;
+			str[0] -= 32;
+		}
+		if (str[i] == 32 && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+		{
+			str[i + 1] -= 32;
 		}
 	}
-	printf("So tu trong chuoi : %d", Space_num+1);
-
-
+	printf("%s\n", str);
 
 }	
