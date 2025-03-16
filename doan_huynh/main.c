043d17ce3d8bca820b21ac54139e87c4c66563d1
DoanHuynh1990 <longway1990@gmail.com>
2025-03-16 20:22:42 +0700

    String_Bai6

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -2,24 +2,17 @@
 #include<stdio.h>
 int main()
 {
-	char str[] = "xin chao moi nguoi ^%$";
+	char str[] = "xin vhao moi nguoi ^%$";
 	int str_len = 0;
-	int Space_num = 0;
 
 	while (str[str_len] != 0) {
 		str_len++;
 	}
 	for (int i = 1; i < str_len; i++)
 	{
-		if (str[0] >= 'a' && str[0] <= 'z')
-		{
-			str[0] -= 32;
-		}
-		if (str[i] == 32 && str[i + 1] >= 'a' && str[i + 1] <= 'z')
-		{
-			str[i + 1] -= 32;
-		}
+		if (str[i]=='c')
+			printf("Vi tri chu c: %d", i);
 	}
-	printf("%s\n", str);
+	
 
 }	
