2fb446066fc85e18a9454f6c93571db4e30ed59f
DoanHuynh1990 <longway1990@gmail.com>
2025-03-17 19:34:11 +0700

    String_Bai8

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -1,18 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int main()
+void main()
 {
-	char str[] = "xin vhao moi nguoi ^%$";
-	int str_len = 0;
+	int a, b;
 
-	while (str[str_len] != 0) {
-		str_len++;
-	}
-	for (int i = 1; i < str_len; i++)
-	{
-		if (str[i]=='c')
-			printf("Vi tri chu c: %d", i);
-	}
-	
+	printf("Input a value: ");
+	scanf_s("%d", &a);
+	printf("Input b value: ");
+	scanf_s("%d", &b);
+
+	printf("Hien thi a,b truoc hoan vi: %d,%d\n",a,  b);
+
+	a = a + b;
+	b = a - b;
+	a = a - b;
+
+	printf("Hien thi a,b sau hoan vi: %d,%d", a, b);
 
 }	
