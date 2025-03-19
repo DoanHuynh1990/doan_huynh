ab653c26549a47f12d45d7149ec838ba88140184
DoanHuynh1990 <longway1990@gmail.com>
2025-03-19 20:56:21 +0700

    Array_Bai4(build_function)

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -1,35 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-// Khai bao ham In ra so lon nhat trong man
-int VT_max(char*dia_chi,int so_luong)
+// Khai bao ham Thay the cac so am trong mang bang 0
+int Minus_change(char*dia_chi,int so_luong)
 {
-	int max = 0;
+	
 	for (int i = 0; i < so_luong; i++)
 	{
-
-		if (max<*(dia_chi+i))
-			max= *(dia_chi + i);
+		if (*(dia_chi+i)<0)
+			 *(dia_chi + i)=0;
 		
 	}
 	
-
-	return max;
-
 }
 
 
 void main()
 {
-	
-
-	 char mang1[] = { 1,2,20,15};
+	 char mang1[] = { 1,-2,20,15};
+	 Minus_change(mang1, 4);
 	 for (int i = 0; i < sizeof(mang1)/sizeof(mang1[0]); i++)
 	 {
-		 if (mang1[i] == VT_max(mang1, 4))
-			 printf("Vi tri so lon nhat:%d", i);
-
+		 printf("%d ", mang1[i]);
 	 }
-
 
 }	
