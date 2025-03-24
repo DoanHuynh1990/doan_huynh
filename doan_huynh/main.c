bbd5b7afe19405be427afc76a2512ec609be3bb5
DoanHuynh1990 <longway1990@gmail.com>
2025-03-24 21:10:36 +0700

    String_Bai6( build_function)

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -1,10 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include"toan.h"
+
+
+int Len(char* str)
+{
+    int diem = 0;
+    while (str[diem] != 0)
+    {
+        diem++;
+
+    }
+    return diem;
+}
+
+
+char VitriC(char* str,char c)
+{
+    int len_chuoi = Len(str);
+    for (int i = 0; i < len_chuoi; i++)
+    {
+        if (str[i] == c) {
+            return str + i;
+        }
+          
+    }
+    return 0;
+          
+}
+
+
 void main()
 {
-	int x = Nhan(6, 4);
-	printf("Ket qua: %d ", x);
-
-
+    char str[] = "xin chao moi nguoi";
+   
+   int x = VitriC(str,'n');
+   printf("%d", x);
+       
 }	
