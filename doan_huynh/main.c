c96f5790fec46d0e9684a12b15e37c92d5a6374d
DoanHuynh1990 <longway1990@gmail.com>
2025-03-26 19:35:26 +0700

    Struct_Bai4

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -7,10 +7,10 @@ typedef struct
     int mau;
  }phan_so;
 
-phan_so divide_fractions(phan_so a, phan_so b) {
+phan_so add_fractions(phan_so a, phan_so b) {
     phan_so result;
-    result.tu = a.tu * b.mau;
-    result.mau = a.mau * b.tu;
+    result.tu = a.tu * b.mau + b.tu * a.mau;
+    result.mau = a.mau * b.mau;
 
     return result;
 
@@ -21,8 +21,8 @@ void main()
     phan_so ps1 = { 2, 3 };
     phan_so ps2 = { 4, 5 };
 
-    phan_so result_divide = divide_fractions(ps1, ps2);
-    printf("Ket qua chia phan so: %d/%d\n", result_divide.tu, result_divide.mau);
+    phan_so result_add = add_fractions(ps1, ps2);
+    printf("Ket qua cong phan so: %d/%d\n", result_add.tu, result_add.mau);
 
 
 
