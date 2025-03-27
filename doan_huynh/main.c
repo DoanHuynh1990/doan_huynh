beb48c17cb3a77217cdef9655445e1f91b1378b3
DoanHuynh1990 <longway1990@gmail.com>
2025-03-27 19:55:51 +0700

    Led_check

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -2,22 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <stdio.h>
-#include <string.h>
-
 int led_check(char* input) {
-    if (strstr(input, "led_2: OFF"))  {
+    char* tu_khoa = "led_2: ";
+    char* a = strstr(input, tu_khoa);
+    
+    a += strlen(tu_khoa); 
+    if (a[0] == 'O' && a[1] == 'N') {
         return 1;
     }
     return 0;
 }
 
-void main() {
-    char* str = "led_1: ON,led_2: OFF, led_3: ON";
+int main() {
+
+    char *str = "led_1: ON, led_2: ON, led_3: ON";
    
 
-    printf("KQ: %d\n", led_check(str)); 
-    
+    printf("Test : %d\n", led_check(str)); 
+ 
 
    
 }
