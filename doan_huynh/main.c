1622c22d046f1226a4f46ab02a9cc643bb0bf7bd
DoanHuynh1990 <longway1990@gmail.com>
2025-03-31 20:01:07 +0700

    Linkedlist_Bai4

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -1,13 +1,11 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct Node {
     int value;
     struct Node* previous_node;
     struct Node* next_node;
 } node_t;
-
 
 typedef struct {
     node_t* last_node;
@@ -15,34 +13,42 @@ typedef struct {
     int len;
 } linked_list_t;
 
+void InsertNodeAtIndex(linked_list_t* ll, int val, int index) {
+    if (index < 0 || index > ll->len) return;
 
-void AddNodeToList(linked_list_t* ll, int val) {
     node_t* node = malloc(sizeof(node_t));
     node->value = val;
-    node->previous_node = ll->last_node;
-    node->next_node = NULL;
 
-    if (ll->last_node) ll->last_node->next_node = node;
-    else ll->head = node;
+    if (index == 0) {
+        node->next_node = ll->head;
+        node->previous_node = NULL;
+        if (ll->head) ll->head->previous_node = node;
+        ll->head = node;
+        if (ll->len == 0) ll->last_node = node;
+    }
+    else {
+        node_t* temp = ll->head;
+        for (int i = 0; i < index - 1; i++) temp = temp->next_node;
+        node->next_node = temp->next_node;
+        node->previous_node = temp;
+        if (temp->next_node) temp->next_node->previous_node = node;
+        else ll->last_node = node;
+        temp->next_node = node;
+    }
 
-    ll->last_node = node;
     ll->len++;
 }
 
-
-int GetLen(linked_list_t* ll) {
-    return ll->len;
-}
-
-
-void main() {
+int main() {
     linked_list_t ll = { 0 };
 
-    AddNodeToList(&ll, 10);
-    AddNodeToList(&ll, 20);
-    AddNodeToList(&ll, 30);
+    InsertNodeAtIndex(&ll, 10, 0);
+    InsertNodeAtIndex(&ll, 20, 1);
+    InsertNodeAtIndex(&ll, 30, 2);
+    InsertNodeAtIndex(&ll, 15, 1);
+    InsertNodeAtIndex(&ll, 5, 0);
+    InsertNodeAtIndex(&ll, 40, 5);
 
-    printf("So luong node: %d\n", GetLen(&ll));
-
+    printf("So luong node: %d\n", ll.len);
     return 0;
 }
