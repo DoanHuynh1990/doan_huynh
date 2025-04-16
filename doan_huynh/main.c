2f00b2ceef00869ae5326a4d32da858a4789ba8f
DoanHuynh1990 <longway1990@gmail.com>
2025-04-16 20:31:58 +0700

    FILE_Bai1

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -1,43 +1,36 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <stdlib.h>
-#include <Windows.h>
+#include <string.h>
 
-
-DWORD WINAPI function_1(_In_ LPVOID lpParameter)
-{
-    while (1) {
-        printf("funtion is running...\n");
-        Sleep(1000);
-
-    }
-}
-
-DWORD WINAPI function_2(_In_ LPVOID lpParameter)
-{
-    while (1) {
-        printf("funtion is running...\n");
-        Sleep(500);
-
-    }
-
-}
-
+typedef struct {
+    char ten[100];
+    int tuoi;
+    char gioi_tinh[10];
+    int mon1, mon2;
+    float dtb;
+} hocsinh_t;
 void main() {
-    HANDLE thread_1= CreateThread(NULL,1024,function_1,NULL,0,NULL);
-    HANDLE thread_2 = CreateThread(NULL, 1024, function_2, NULL, 0, NULL);
-    int cnt = 0;
+    FILE* pf = fopen("C:\\Users\\win10\\Downloads\\DanhSachHocSinh1.csv", "r");
+    if (pf==NULL) {
+        printf("mo file that bai!\n");
+        return;
+    }
 
-    while (1) 
-    {
-        printf("main is running...\n");
-        Sleep(1000);
-        if (cnt++ == 5) 
-        {
-            SuspendThread(thread_1);
-            SuspendThread(thread_2);
-
+    hocsinh_t hs_max = { "", 0, "", 0, 0, 0.0 };
+    hocsinh_t hs;
+      
+    while (fscanf(pf, "%[^,],%d,%[^,],%d,%d\n", hs.ten, &hs.tuoi, hs.gioi_tinh, &hs.mon1, &hs.mon2)==5) {
+          hs.dtb = (float)(hs.mon1 + hs.mon2) / 2;
+        if (hs.dtb > hs_max.dtb) {
+            hs_max = hs;
         }
     }
- }
 
+    fclose(pf);
 
+    printf("Hoc sinh co diem TB cao nhat:\n");
+    printf("Ten: %s\n", hs_max.ten);
+    printf("Diem TB: %.2f\n", hs_max.dtb);
+
+}
