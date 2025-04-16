34169d0e1480f1e16c09d922b7a7b3db0e111693
DoanHuynh1990 <longway1990@gmail.com>
2025-04-16 20:39:12 +0700

    FILE_Bai2

--- doan_huynh/main.c
+++ doan_huynh/main.c
@@ -10,27 +10,40 @@ typedef struct {
     int mon1, mon2;
     float dtb;
 } hocsinh_t;
+
 void main() {
     FILE* pf = fopen("C:\\Users\\win10\\Downloads\\DanhSachHocSinh1.csv", "r");
-    if (pf==NULL) {
-        printf("mo file that bai!\n");
+    if (pf == NULL) {
+        printf("Mo file that bai!\n");
         return;
     }
 
-    hocsinh_t hs_max = { "", 0, "", 0, 0, 0.0 };
+    hocsinh_t danh_sach_hs[100];  
+    int so_hoc_sinh = 0;
     hocsinh_t hs;
-      
-    while (fscanf(pf, "%[^,],%d,%[^,],%d,%d\n", hs.ten, &hs.tuoi, hs.gioi_tinh, &hs.mon1, &hs.mon2)==5) {
-          hs.dtb = (float)(hs.mon1 + hs.mon2) / 2;
-        if (hs.dtb > hs_max.dtb) {
-            hs_max = hs;
-        }
+
+    while (fscanf(pf, "%[^,],%d,%[^,],%d,%d\n", hs.ten, &hs.tuoi, hs.gioi_tinh, &hs.mon1, &hs.mon2) == 5) {
+        hs.dtb = (float)(hs.mon1 + hs.mon2) / 2;
+        danh_sach_hs[so_hoc_sinh] = hs;
+        so_hoc_sinh++;
     }
 
     fclose(pf);
 
-    printf("Hoc sinh co diem TB cao nhat:\n");
-    printf("Ten: %s\n", hs_max.ten);
-    printf("Diem TB: %.2f\n", hs_max.dtb);
+     for (int i = 0; i < so_hoc_sinh - 1; i++) {
+        for (int j = 0; j < so_hoc_sinh - i - 1; j++) {
+            if (danh_sach_hs[j].dtb < danh_sach_hs[j + 1].dtb) {
+                                hocsinh_t temp = danh_sach_hs[j];
+                danh_sach_hs[j] = danh_sach_hs[j + 1];
+                danh_sach_hs[j + 1] = temp;
+            }
+        }
+    }
 
+       printf("Danh sach hoc sinh theo diem TB tu cao den thap:\n");
+    for (int i = 0; i < so_hoc_sinh; i++) {
+        printf("Ten: %s\n", danh_sach_hs[i].ten);
+        printf("Diem TB: %.2f\n", danh_sach_hs[i].dtb);
+        printf("\n");
+    }
 }
