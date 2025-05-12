b0f9b139e9537a1b5aaa81e8644f450063e7de64
DoanHuynh1990 <longway1990@gmail.com>
2025-05-12 19:44:31 +0700

    C++.Rut gon phan so

--- doan_huynh/main.cpp
+++ doan_huynh/main.cpp
@@ -1,28 +1,37 @@
 ﻿#include <stdio.h>
 
+
+int ucln(int a, int b) {
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+
 class phan_so {
 public:
     int tu_so;
     int mau_so;
 
-    void tru(phan_so khac) {
-        tu_so = tu_so * khac.mau_so - khac.tu_so * mau_so;
-        mau_so = mau_so * khac.mau_so;
+  
+    void rut_gon() {
+        int uoc_chung_lon_nhat = ucln(tu_so, mau_so);
+        tu_so /= uoc_chung_lon_nhat;
+        mau_so /= uoc_chung_lon_nhat;
     }
 };
 
-void main() {
+int main() {
     phan_so a;
-    a.tu_so = 5;
-    a.mau_so = 6;
+    a.tu_so = 6;
+    a.mau_so = 9;
 
-    phan_so b;
-    b.tu_so = 1;
-    b.mau_so = 3;
+    a.rut_gon();  
 
-    a.tru(b);  
-
-    printf("result: %d/%d\n", a.tu_so, a.mau_so);
+    printf("result sau rut gon: %d/%d\n", a.tu_so, a.mau_so);
 
 
 }
