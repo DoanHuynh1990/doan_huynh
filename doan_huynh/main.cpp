614c870c6244eec2f81dc61e7529171eecea6479
DoanHuynh1990 <longway1990@gmail.com>
2025-05-12 19:48:09 +0700

    C++.So sanh phan so

--- doan_huynh/main.cpp
+++ doan_huynh/main.cpp
@@ -1,37 +1,28 @@
 ﻿#include <stdio.h>
 
 
-int ucln(int a, int b) {
-    while (b != 0) {
-        int r = a % b;
-        a = b;
-        b = r;
-    }
-    return a;
-}
-
-
 class phan_so {
 public:
     int tu_so;
     int mau_so;
 
-  
-    void rut_gon() {
-        int uoc_chung_lon_nhat = ucln(tu_so, mau_so);
-        tu_so /= uoc_chung_lon_nhat;
-        mau_so /= uoc_chung_lon_nhat;
+   
+    bool lon_hon(phan_so khac) {
+        return (tu_so * khac.mau_so > khac.tu_so * mau_so);
     }
 };
 
-int main() {
-    phan_so a;
-    a.tu_so = 6;
-    a.mau_so = 9;
+void main() {
+    phan_so a, b;
+    a.tu_so = 3;  a.mau_so = 4;  
+    b.tu_so = 2;  b.mau_so = 3;   
 
-    a.rut_gon();  
-
-    printf("result sau rut gon: %d/%d\n", a.tu_so, a.mau_so);
+    if (a.lon_hon(b)) {
+        printf("Phan so a lon hon phan so b\n");
+    }
+    else {
+        printf("Phan so a khong lon hon phan so b\n");
+    }
 
 
 }
