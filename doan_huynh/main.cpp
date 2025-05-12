cc51f40b592a6b2285dea0981a293ed713ce8587
DoanHuynh1990 <longway1990@gmail.com>
2025-05-12 19:37:36 +0700

    C++.Cong phan so

--- doan_huynh/main.cpp
+++ doan_huynh/main.cpp
@@ -5,13 +5,13 @@ public:
     int tu_so;
     int mau_so;
 
-    void chia(phan_so khac) {
-        tu_so = tu_so * khac.mau_so;
-        mau_so = mau_so * khac.tu_so;
+    void cong(phan_so khac) {
+        tu_so = tu_so * khac.mau_so + khac.tu_so * mau_so;
+        mau_so = mau_so * khac.mau_so;
     }
 };
 
-void  main() {
+void main() {
     phan_so a;
     a.tu_so = 1;
     a.mau_so = 2;
@@ -20,7 +20,7 @@ void  main() {
     b.tu_so = 3;
     b.mau_so = 4;
 
-    a.chia(b);  
+    a.cong(b); 
 
     printf("result: %d/%d\n", a.tu_so, a.mau_so);
 
