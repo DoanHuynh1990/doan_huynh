2f594cf4bbe4ded19c5394fcc92a91a4c1c89913
DoanHuynh1990 <longway1990@gmail.com>
2025-05-12 19:39:49 +0700

    C++.Tru phan so

--- doan_huynh/main.cpp
+++ doan_huynh/main.cpp
@@ -5,22 +5,22 @@ public:
     int tu_so;
     int mau_so;
 
-    void cong(phan_so khac) {
-        tu_so = tu_so * khac.mau_so + khac.tu_so * mau_so;
+    void tru(phan_so khac) {
+        tu_so = tu_so * khac.mau_so - khac.tu_so * mau_so;
         mau_so = mau_so * khac.mau_so;
     }
 };
 
 void main() {
     phan_so a;
-    a.tu_so = 1;
-    a.mau_so = 2;
+    a.tu_so = 5;
+    a.mau_so = 6;
 
     phan_so b;
-    b.tu_so = 3;
-    b.mau_so = 4;
+    b.tu_so = 1;
+    b.mau_so = 3;
 
-    a.cong(b); 
+    a.tru(b);  
 
     printf("result: %d/%d\n", a.tu_so, a.mau_so);
 
